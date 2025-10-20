a37dd7f2cf523724f003aaf3b2dba7a638fc726f
Ömer Deliavcı <omerdeliavci@outlook.com>
2025-10-20 11:38:52 +0300

    venny direct

--- components/modal/modals/index.js
+++ components/modal/modals/index.js
@@ -1,9 +1,9 @@
-import Contact_Modal from "./contact-modal";
 import Settings_Modal from "./settings-modal";
+import Contact_Modal from "./contact-modal";
 import Watch_Modal from "./watch-modal";
 
 export const MODAL_COMPONENTS = {
   SETTINGS_MODAL: Settings_Modal,
-  WATCH_MODAL: Watch_Modal,
   CONTACT_MODAL: Contact_Modal,
+  WATCH_MODAL: Watch_Modal,
 };
