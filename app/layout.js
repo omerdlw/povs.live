3cf37b22e43bee3cab649111e65bb360daa38748
Ömer Deliavcı <omerdeliavci@outlook.com>
2025-10-20 20:24:08 +0300

    fix

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ import Countdown from "@/components/countdown";
 
 export default function RootLayout({ children }) {
   const pathname = usePathname();
-  const isCountdownActive = process.env.NEXT_PUBLIC_COUNTDOWN;
+  const isCountdownActive = false;
 
   return (
     <html lang="en">
